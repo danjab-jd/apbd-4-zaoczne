725409f558a8a0445ef2bf94625045fcc8b614eb
danjab-jd <jd@pja.edu.pl>
2022-05-01 10:31:37 +0200

    Added example with transactions

---
+++ Transactions/Transactions/Controllers/AnimalsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Transactions.Models;
+using Transactions.Services;
+
+namespace Transactions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly IAnimalDbService _animalDbService;
+
+        public AnimalsController(IAnimalDbService animalsDbService)
+        {
+            _animalDbService = animalsDbService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAnimal(AnimalDTO animal)
+        {
+            MethodResult result = await _animalDbService.AddAnimalAsync(animal);
+
+            return StatusCode((int)result.StatusCode, result.Message);
+        }
+    }
+}
---
+++ Transactions/Transactions/Services/IAnimalDbService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Transactions.Models;
+
+namespace Transactions.Services
+{
+    public interface IAnimalDbService
+    {
+        Task<MethodResult> AddAnimalAsync(AnimalDTO animalDTO);
+    }
+}
